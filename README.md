d36e691a78933538edcde3caa1092a3bfdfd571e
Sina Pilehchiha <sina.pilehchiha@trailofbits.com>
2020-05-26 16:50:57 -0400

    Slither is now directly used in the extraction script, instead of as a called subprocess. The script is now called properly according to its location regarding the project to be analyzed in order to avoid Crytic Invalid Compilation errors (missing some dependency files when being called from the wrong path.)

---
+++ README.md
@@ -0,0 +1,1 @@
+The script should be located in the top-level directory as to the project to be analyzed in order to avoid compilation errors.
--- ir_extraction_script.py
+++ ir_extraction_script.py
@@ -1,10 +1,12 @@
 import csv
 import json
 import ast
-import subprocess
+#import subprocess
 import re
-
-csv_input_file_path = "./issues_dataset - Sheet1.csv"
+from slither import Slither
+from slither.printers.all_printers import PrinterSlithIR
+        
+csv_input_file_path = "../../../issues-dataset/issues_dataset - Sheet1.csv"
 
 search_term = input("Enter a function name with the pattern below: \n Functoin [file_name.function_name]:\n")
 
@@ -19,23 +21,25 @@ def csv_to_json(file_path):
 def json_search_and_extraction(file_path, name):
     json_object = csv_to_json(file_path)
     json_element = [obj for obj in json_object if obj['func']==name][0]
-    return "../smart-contract-clients/contracts_audited/" + json_element['project_id'] + '/' + json_element['func_origin_contract']
+    return "../" + json_element['project_id'] + '/' + json_element['func_origin_contract']
 
 def run_slither(slithir_output_path):
-    slithir_output = open(slithir_output_path, 'w')
-    subprocess.call(["slither", "--json", "-", "--print", "slithir", json_search_and_extraction(csv_input_file_path, search_term)], stdout=slithir_output)
+    slithir_output = open(slithir_output_path, 'w')    
+    # Init slither
+    slither = Slither(json_search_and_extraction(csv_input_file_path, search_term))
+    slither.register_printer(PrinterSlithIR)
+    slithir_output.write(json.dumps(slither.run_printers()))
     slithir_output.close()
 
 def load_slithir_data(slithir_output_path):
     run_slither(slithir_output_path)
     with open(slithir_output_path, 'r') as slithir_output:
         json_data = json.load(slithir_output)
-    return json_data['results']['printers'][0]['description']
+    return json_data[0]['description']
 
 def slithir_search_and_extraction(slithir_output_path):
-    regex_pattern = "\\n\\t" + re.escape(search_term) + "\\n\\t\\t(.*?)\\n\\tFunction"
-    string = load_slithir_data(slithir_output_path)
-    return re.search(regex_pattern, string, flags=re.DOTALL).group(1)
+    regex_pattern = "\\n\\t" + re.escape(search_term) + "(.*?)\\n\\tFunction" 
+    return re.search(regex_pattern, load_slithir_data(slithir_output_path), flags=re.DOTALL).group(1)
 
 if __name__ == "__main__":
     output = slithir_search_and_extraction('./slithir.json')
