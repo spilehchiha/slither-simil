20f4c75dda01df1e13d4951a4348a3dee07907e0
Sina Pilehchiha <sina.pilehchiha@trailofbits.com>
2020-05-28 08:23:14 -0400

    Now there is no need for user input as we want everything as automated as possible. I cannot get Slither('.') to work. Will fix in the next commit.

--- ir_extraction_script.py
+++ ir_extraction_script.py
@@ -2,30 +2,27 @@ import json
 from slither import Slither
 
 input_dataset = './vul_dataset.json'
-search_term = input("Enter a function name with the pattern below: \n Functoin [contract_name.function_name]:\n")
 
-def json_search_and_extraction(file_path, name):
+def json_search_and_extraction(file_path):
+    array = list()
     with open(file_path, 'r') as f:
         json_object = json.load(f)
     for row in json_object:
-        if search_term == row['func']:
-            return row['func_origin_contract']
+        array.append(row['func_origin_contract'])
+    return array
 
 def run_slither(slithir_output_path): 
+    data = json_search_and_extraction(input_dataset)
     # Init slither
-    slither = Slither(json_search_and_extraction(input_dataset, search_term))
-    
-    # Extracts the function name without the 'Function' keyword from the user input
-    function_name = search_term.split(r" ")[1]
-
-    for function in slither.functions:
-        if function.canonical_name == function_name:
-            for node in function.nodes:
-                if node.expression:
-                    print('\t\tExpression: {}'.format(node.expression))
-                    print('\t\tIRs:')
-                    for ir in node.irs:
-                        print('\t\t\t{}'.format(ir))
+    for contract_file_name in data:
+        slither = Slither(contract_file_name)
+        for function in slither.functions:
+                for node in function.nodes:
+                    if node.expression:
+                        print('\t\tExpression: {}'.format(node.expression))
+                        print('\t\tIRs:')
+                        for ir in node.irs:
+                            print('\t\t\t{}'.format(ir))
 
 if __name__ == "__main__": 
     run_slither('./slithir.json')
