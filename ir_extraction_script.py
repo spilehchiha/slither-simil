005d89db97aef4eaeb270724c3aa085decc5e7bc
Sina Pilehchiha <sina.pilehchiha@trailofbits.com>
2020-05-28 05:07:28 -0400

    Added back the 'input as .json.' as a separate file.  We do need the input dataset as .json contracry to the previous commit message (miscommuniction.)

--- ir_extraction_script.py
+++ ir_extraction_script.py
@@ -1,27 +1,19 @@
-import csv
+import json
 from slither import Slither
-        
-csv_input_file_path = "../../../issues-dataset/issues_dataset - Sheet1.csv"
 
-search_term = input("Enter a function name with the pattern below: \n Functoin [file_name.function_name]:\n")
-
-def csv_to_json(file_path): 
-    with open (file_path, 'r') as input_file:
-        csv_data = list()
-        for row in csv.DictReader(input_file):
-            csv_data.append(row)
-    input_file.close()
-    return csv_data
+input_dataset = './vul_dataset.json'
+search_term = input("Enter a function name with the pattern below: \n Functoin [contract_name.function_name]:\n")
 
 def json_search_and_extraction(file_path, name):
-    csv_object = csv_to_json(file_path)
-    for row in csv_object:
+    with open(file_path, 'r') as f:
+        json_object = json.load(f)
+    for row in json_object:
         if search_term == row['func']:
             return row['func_origin_contract']
 
 def run_slither(slithir_output_path): 
     # Init slither
-    slither = Slither(json_search_and_extraction(csv_input_file_path, search_term))
+    slither = Slither(json_search_and_extraction(input_dataset, search_term))
     
     # Extracts the function name without the 'Function' keyword from the user input
     function_name = search_term.split(r" ")[1]
@@ -35,5 +27,5 @@ def run_slither(slithir_output_path):
                     for ir in node.irs:
                         print('\t\t\t{}'.format(ir))
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     run_slither('./slithir.json')
